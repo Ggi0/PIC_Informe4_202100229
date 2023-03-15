39df5c12a94489672c986dab08291b8ba26c4a39
Ggi0 <gconcoha21@gmail.com>
2023-03-15 16:46:56 -0600

    backEnd

---
+++ BackEnd/index.js
@@ -0,0 +1,59 @@
+import express from 'express'
+//importar{QUE?} de donde? 'ruta'
+import {test, estudiantes} from './students.js'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+
+
+
+const app = express();
+//Aceptar peticiones de la misma computadora como ambas van a estar en localhost
+app.use(cors({
+    //todos los origenes '*'
+    origin: '*'
+}));
+
+//Que pueda aceptar request tipo json
+app.use(bodyParser.json());
+
+//(Puerto = 5000, llamará un callback funtion -- para sarber si el servido inico)
+app.listen(5000, ()=>{
+    console.log('El servidor ha iniciado');
+});
+
+//END-POINTS
+
+//toda callback funtion de express tiene estos dos parametros (REQuest, RESponds)
+app.get('/', (req, res)=>{
+    res.end('<h1>Hello World</h1>');
+    //la linea de arriba es equivalente a hacer los siguiente:
+    //res.send('<h1>Hello World</h1>');
+    //res.end();
+});
+
+app.get('/about', (req, res)=>{
+    res.end('<h1>Sobre nosotros</h1>');
+
+});
+
+app.get('/estudiantes', (req, res)=>{
+    //1) se manda una respuesta de tipo json:
+    res.json(estudiantes);
+    //
+    res.end();
+})
+
+//all() --> ABARCA TODO LOS METODOS
+//          get();post();put();delete();update()
+//Aplicarlo para todas las rutas
+app.all('*', (req, res)=>{
+    //404 -> error
+    res.status(404).send('<h1>Página no disponible</h1>');
+})
+
+
+
+
+
+
+//app.post();
---
+++ BackEnd/students.js
@@ -0,0 +1,12 @@
+//Arreglo de estudiantes
+
+//export{que quiero exportar} de donde './students.js'
+//import {test, estudiantes} from './students.js
+export const estudiantes = [{"name": "name1","lastName":"lastName1","edad": 20},
+    {"name": "Juan","lastName": "Perez","edad": 25},
+    {"name": "Benaventi","lastName": "Fuentes","edad": 30}
+]
+
+export function test(){
+    console.log('test');
+}
